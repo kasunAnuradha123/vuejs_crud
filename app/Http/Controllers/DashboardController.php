f7b14375dcd722a0a54c8a366edd1f0fbc4a0358
Kasun <anuradhakasun12345@gmail.com>
2025-04-08 14:05:49 +0530

    user table modify

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,19 +17,39 @@ class DashboardController extends Controller
     }
 
     public function create(){
-        return Inertia::render('CreateUser');
+        $user = User::where('status', 'draft')->first();
+        if (!$user) {
+            $user = User::create([
+                'name' => '',
+                'email' => '',
+                'password' => Hash::make('password'), // Default password, you can change this as needed
+                'status' => 'draft',
+            ]);
+        }
+        
+        return Inertia::render('CreateUser', $this->formData($user, "create"));
     }
 
-    public function createUser(Request $request){
+    protected function formData( $user, $type)
+    {
+        return [
+            'user' => $user,
+            'type' => $type,
+        ];
+    }
+
+
+    public function updateUser(Request $request, User $user){
         $request->validate([
             'name' => 'required|string|min:5',
-            'email' => 'required|string|email|max:255|unique:users',
+            'email' => 'required|string|email|max:255|unique:users,email,' . $user->id,
         ]);
 
         User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make('password'), // Default password, you can change this as needed
+            'status' => 'active',
         ]);
 
         return redirect()->route('dashboard')->with('success', 'User created successfully.');
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,66 +1,54 @@
 <script setup lang="ts">
-import NavLink from '@/Components/NavLink.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { User } from '@/types';
-import { Head } from '@inertiajs/vue3';
+    import NavLink from '@/Components/NavLink.vue';
+    import PrimaryButton from '@/Components/PrimaryButton.vue';
+    import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+    import { User } from '@/types';
+    import { Head } from '@inertiajs/vue3';
 
 
-defineProps<{
-    users?:User[];
-}>();
+    defineProps<{
+        users?: User[];
+    }>();
 
 </script>
 
 <template>
+
     <Head title="Dashboard" />
 
     <AuthenticatedLayout>
         <template #header>
-            <h2
-                class="text-xl font-semibold leading-tight text-gray-800"
-            >
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
                 Dashboard
             </h2>
         </template>
 
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div
-                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
-                >
-                <dev class=" grid grid-cols-2"> 
-                    <div class="px-6 py-4">
-                        <!-- <ResponsiveNavLink :href="route('profile.edit')">
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <dev class=" grid grid-cols-2">
+                        <div class="px-6 py-4">
+                            <!-- <ResponsiveNavLink :href="route('profile.edit')">
                             Profile
                         </ResponsiveNavLink> -->
-                     <NavLink
-                            :href="route('create')"
-                            class="text-black bg-indigo-300 rounded-md px-2 "
-                        >
-                            Add Users
-                        </NavLink>
-                    </div>
+                            <NavLink :href="route('create')" class="text-black bg-indigo-300 rounded-md px-2 ">
+                                Add Users
+                            </NavLink>
+                        </div>
                     </dev>
                     <table class="min-w-full divide-y divide-gray-200">
                         <thead class="bg-gray-50">
                             <tr>
-                                <th
-                                    scope="col"
-                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                >
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                     Name
                                 </th>
-                                <th
-                                    scope="col"
-                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                >
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                     Email
                                 </th>
-                                <th
-                                    scope="col"
-                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                >
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                     Created At
                                 </th>
                             </tr>
@@ -76,37 +64,21 @@ defineProps<{
                                 <td class="px-6 py-4 whitespace-nowrap">
                                     {{ user.created_at }}
                                 </td>
+
                                 <td class="px-6 py-4 whitespace-nowrap">
-                                    <NavLink
-                                        :href="route('deleteUser', { id: user.id })"
-                                        method="delete"
-                                        as="button"
-                                        class="text-white bg-red-500 rounded-md px-2 py-1"
-                                    >
-                                        Delete
-                                    </NavLink>
-                                    <!-- <NavLink
-                                        :href="route('deleteUser', user.id,{
-                                            method: 'delete',
-                                        })"
-                                        class="text-black bg-indigo-300 rounded-md px-2 "
-                                    >
-                                        Delete
-                                    </NavLink> -->
-                                </td>
-                                <!-- delete button  -->
-                                <!-- <td class="px-6 py-4 whitespace-nowrap">
-                                    <form
-                                        @submit.prevent="form.delete(route('deleteUser', user.id))"
-                                    >
-                                        <PrimaryButton
-                                            type="submit"
-                                            class="bg-red-500 text-white rounded-md px-2"
-                                        >
+                                    <div class="grid grid-cols-2">
+                                        <NavLink :href="route('deleteUser', { id: user.id })" method="delete"
+                                            as="button" class="text-white mx-auto px-5 bg-blue-500 rounded-md">
+                                            Edit
+                                        </NavLink>
+                                        <NavLink :href="route('deleteUser', { id: user.id })" method="delete"
+                                            as="button" class="text-white mx-auto bg-red-500 rounded-md px-4">
                                             Delete
-                                        </PrimaryButton>
-                                    </form>
-                                </td> -->
+                                        </NavLink>
+                                    </div>
+
+                                </td>
+
                             </tr>
                         </tbody>
                     </table>
